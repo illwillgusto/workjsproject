054af721dcc617cf30ac4c2b73a16fed160eb763
Will Walton <will@endless3dge.com>
2024-05-01 12:55:06 -0500

    Update index.js
    added the employee object
    declared the firstname variable and the prompt value to enter the first name
    added a condition if the user doesn't enter the first name along with the process to exit the application

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import createPrompt from 'prompt-sync';
 let prompt = createPrompt();
-// test
+
+let employee = {};
+
+let firstName = prompt("First Name: ");
+if (!firstName) {
+  console.error('Invalid first name'); // this will appear if a user doesn't enter their first name
+  process.exit(1); // exiting the application
+}
+employee.firstName = firstName;
